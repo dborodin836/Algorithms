ddd1ab1ceff5b630f6b01243be4307770bb636de
dborodin836 <dborodin836@gmail.com>
2021-12-12 23:35:11 +0200

    Changes in testing

--- Sorts/__init__.py
+++ Sorts/__init__.py
@@ -4,19 +4,13 @@ from SelectionSort import *
 from CountSort import *
 
 if __name__ == '__main__':
-    assert bubble([5, 1, 0, 122, 12, -100, 200]) == [-100, 0, 1, 5, 12, 122, 200]
-    a = [5, 1, 0, 122, 12, -100, 200]
-    bubble_sorted(a)
-    assert a == [-100, 0, 1, 5, 12, 122, 200]
-    assert selection([5, 1, 0, 122, 12, -100, 200]) == [-100, 0, 1, 5, 12, 122, 200]
-    a = [5, 1, 0, 122, 12, -100, 200]
-    selection_sorted(a)
-    assert a == [-100, 0, 1, 5, 12, 122, 200]
-    assert insertion([5, 1, 0, 122, 12, -100, 200]) == [-100, 0, 1, 5, 12, 122, 200]
-    a = [5, 1, 0, 122, 12, -100, 200]
-    insertion_sorted(a)
-    assert a == [-100, 0, 1, 5, 12, 122, 200]
-    assert count([5, 1, 0, 122, 12, -100, 200]) == [-100, 0, 1, 5, 12, 122, 200]
-    a = [5, 1, 0, 122, 12, -100, 200]
-    count_sorted(a)
-    assert a == [-100, 0, 1, 5, 12, 122, 200]
+    for func in [bubble, bubble_sorted, count, count_sorted,
+                 insertion, insertion_sorted, selection, selection_sorted]:
+
+        if str(func).endswith('_sorted'):
+            assert func([5, 1, 0, 122, 12, -100, 200]) == [-100, 0, 1, 5, 12, 122, 200]
+
+        if str(func).endswith('_sorted'):
+            a = [5, 1, 0, 122, 12, -100, 200]
+            func(a)
+            assert a == [-100, 0, 1, 5, 12, 122, 200]
