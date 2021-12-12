c5c67c469f2da04572bef29315e6ce29c982eeab
dborodin836 <dborodin836@gmail.com>
2021-12-12 23:35:11 +0200

    Changes in testing

--- Sorts/__init__.py
+++ Sorts/__init__.py
@@ -7,10 +7,10 @@ if __name__ == '__main__':
     for func in [bubble, bubble_sorted, count, count_sorted,
                  insertion, insertion_sorted, selection, selection_sorted]:
 
-        if str(func).endswith('_sorted'):
+        if 'sorted' not in str(func):
             assert func([5, 1, 0, 122, 12, -100, 200]) == [-100, 0, 1, 5, 12, 122, 200]
 
-        if str(func).endswith('_sorted'):
-            a = [5, 1, 0, 122, 12, -100, 200]
-            func(a)
-            assert a == [-100, 0, 1, 5, 12, 122, 200]
+        if 'sorted' in str(func):
+            lst = [5, 1, 0, 122, 12, -100, 200]
+            func(lst)
+            assert lst == [-100, 0, 1, 5, 12, 122, 200]
