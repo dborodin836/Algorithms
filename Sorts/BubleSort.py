a1a9134c81413ddafe17b84fbfa772b2c0321740
PootisMaan <dborodin836@gmail.com>
2021-12-22 09:56:36 +0200

    no to sorted division

--- Sorts/BubleSort.py
+++ Sorts/BubleSort.py
@@ -1,14 +1,5 @@
-def bubble_sorted(a_list: list):
+def bubble(a_list: list):
     for j in range(len(a_list)):
         for i in range(1, len(a_list) - j):
             if a_list[i - 1] > a_list[i]:
                 a_list[i - 1], a_list[i] = a_list[i], a_list[i - 1]
-
-
-def bubble(a_list: list) -> list:
-    b_list = a_list[:]
-    for j in range(len(b_list)):
-        for i in range(1, len(b_list) - j):
-            if b_list[i - 1] > b_list[i]:
-                b_list[i - 1], b_list[i] = b_list[i], b_list[i - 1]
-    return b_list
--- Sorts/__init__.py
+++ Sorts/__init__.py
@@ -4,13 +4,7 @@ from SelectionSort import *
 from CountSort import *
 
 if __name__ == '__main__':
-    for func in [bubble, bubble_sorted, count, count_sorted,
-                 insertion, insertion_sorted, selection, selection_sorted]:
-
-        if 'sorted' not in str(func):
-            assert func([5, 1, 0, 122, 12, -100, 200]) == [-100, 0, 1, 5, 12, 122, 200]
-
-        if 'sorted' in str(func):
-            lst = [5, 1, 0, 122, 12, -100, 200]
-            func(lst)
-            assert lst == [-100, 0, 1, 5, 12, 122, 200]
+    for func in [bubble, count, insertion, selection]:
+        lst = [5, 1, 0, 122, 12, -100, 200]
+        func(lst)
+        assert lst == [-100, 0, 1, 5, 12, 122, 200]
